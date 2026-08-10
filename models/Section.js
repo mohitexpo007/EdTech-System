2443537f7f0d9fd7ffc13f21f65865a4aae97efc
Mohit <arceus930076@gmail.com>
2026-08-11 00:21:08 +0530

    created the OTP model and tags model

--- models/Section.js
+++ models/Section.js
@@ -5,12 +5,12 @@ const sectionSchema=new mongoose.Schema({
   sectionName:{
     type:String
   },
-  subSection:{
+  subSection:[{
     type:mongoose.Schema.Types.ObjectId,
     required:true,
     ref:"SubSection"
-  }
-  
+  }]
+
 });
 
 module.exports=mongoose.model("Section",sectionSchema);
